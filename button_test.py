9267cd64c45a822c93862dbf13941ee9d230afd3
Tom Lambrechts <tola2000@gmail.com>
2025-11-13 00:39:00 +0100

    dddd

--- button_test.py
+++ button_test.py
@@ -3,23 +3,29 @@ import busio
 from adafruit_mcp230xx.mcp23017 import MCP23017
 import time
 
+# I2C setup
 i2c = busio.I2C(board.SCL, board.SDA)
 mcp = MCP23017(i2c, address=0x27)
 
+# Configure GPA0 as input with internal pull-up
 button_pin = mcp.get_pin(0)
 button_pin.switch_to_input(pullup=True)
 
-button_was_pressed = False
+button_pressed = False
+debounce_time = 0.2  # 200 ms debounce
+last_press_time = 0
 
 try:
     print("Monitoring Button 0 (GPA0)... Press to test.")
     while True:
-        if not button_pin.value and not button_was_pressed:
-            print("Button 0 pressed!")
-            button_was_pressed = True
-        elif button_pin.value and button_was_pressed:
-            # button released
-            button_was_pressed = False
-        time.sleep(0.05)
+        current_time = time.time()
+        if not button_pin.value and not button_pressed:
+            if current_time - last_press_time > debounce_time:
+                print("Button 0 pressed!")
+                button_pressed = True
+                last_press_time = current_time
+        elif button_pin.value and button_pressed:
+            button_pressed = False
+        time.sleep(0.01)
 except KeyboardInterrupt:
     print("Exiting...")
