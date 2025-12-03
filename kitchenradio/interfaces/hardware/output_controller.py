2af8ab6a994bfe2a947585856d6bc97b3f346d49
TOLA2000 <tola2000@gmail.com>
2025-12-03 01:56:28 +0100

    xcvxwv

--- kitchenradio/interfaces/hardware/output_controller.py
+++ kitchenradio/interfaces/hardware/output_controller.py
@@ -13,6 +13,7 @@ if TYPE_CHECKING:
     from kitchenradio.sources.source_controller import SourceController
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 # Hardware configuration flag
 try:
