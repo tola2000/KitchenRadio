1fa55fd628af673c04f849d290799be6475fdbd9
TOLA2000 <tola2000@gmail.com>
2025-12-03 16:17:45 +0100

    sdfsqdf

--- kitchenradio/interfaces/hardware/display_formatter.py
+++ kitchenradio/interfaces/hardware/display_formatter.py
@@ -832,7 +832,7 @@ class DisplayFormatter:
         
         # Triangle pointing DOWN (point at bottom, 90° angle at bottom, 45° slopes)
         # Top edge of triangle connects the bottom of the circles
-        triangle_top_y = circles_cy + (circle_radius // 4 ) + 2   # Moved down 3 pixels
+        triangle_top_y = circles_cy + (circle_radius // 4 ) + 4   # Moved down 3 pixels
         triangle_left_x = left_circle_cx - ( circle_radius   // 2 ) 
         triangle_right_x = right_circle_cx + ( circle_radius // 2 ) 
         
@@ -842,7 +842,7 @@ class DisplayFormatter:
         triangle_height = int(triangle_base / 2) 
         
         triangle_bottom_x = cx
-        triangle_bottom_y = triangle_top_y + triangle_height + 2
+        triangle_bottom_y = triangle_top_y + triangle_height + 4
         
         # Calculate bounding box
         min_x = left_circle_cx - circle_radius
